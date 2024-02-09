3cb241031d9eaa6f184d96b79b5c8e0020272ccc
eunbin <eunbin2158@gmail.com>
2024-02-09 15:51:04 +0900

    appbarbottomN update

--- lib/ResultPage.dart
+++ lib/ResultPage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:brain/MapPage.dart';
 
+import 'ChatBotPage.dart';
+import 'ChoicePage.dart';
+
 class ResultPage extends StatefulWidget {
   const ResultPage({Key? key}) : super(key: key);
 
@@ -12,22 +15,44 @@ class _ResultPageState extends State<ResultPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // appBar: AppBar(
+      //   title: const Text('결과 페이지'),
+      //   leading: IconButton(
+      //     icon: const Icon(Icons.home),
+      //     onPressed: () {
+      //       Navigator.popUntil(context, ModalRoute.withName('/'));
+      //     },
+      //   ),
+      //   actions: [
+      //     IconButton(
+      //       icon: const Icon(Icons.map),
+      //       onPressed: () {
+      //         Navigator.push(
+      //           context,
+      //           MaterialPageRoute(builder: (context) => const MapPage()),
+      //         );
+      //       },
+      //     ),
+      //   ],
+      // ),
       appBar: AppBar(
-        title: const Text('결과 페이지'),
-        leading: IconButton(
-          icon: const Icon(Icons.home),
-          onPressed: () {
-            Navigator.popUntil(context, ModalRoute.withName('/'));
-          },
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center, // 제목을 가운데 정렬합니다.
+          children: [
+            IconButton(
+              icon: Image.asset('MainLogo.png'), // 로고 이미지
+              onPressed: () {
+                // 로고를 클릭했을 때 실행될 동작
+                Navigator.of(context).popUntil((route) => route.isFirst);
+              },
+            ),
+          ],
         ),
-        actions: [
+        actions: <Widget>[
           IconButton(
-            icon: const Icon(Icons.map),
+            icon: Image.asset('UserIcon.png'), // 사용자 아이콘 이미지
             onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const MapPage()),
-              );
+              // 사용자 아이콘을 클릭했을 때 실행될 동작을 여기에 작성합니다.
             },
           ),
         ],
@@ -46,6 +71,55 @@ class _ResultPageState extends State<ResultPage> {
             ResultSection(title: '필요성'),
           ],
         ),
+      ),
+      bottomNavigationBar:BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, // 아이템들이 동일한 공간을 차지하도록 설정
+        selectedItemColor: Colors.black, // 선택된 아이템의 색상을 검정으로 설정
+        unselectedItemColor: Colors.black, // 선택되지 않은 아이템의 색상을 검정으로 설정
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('mapIcon.png'), color: Colors.black),
+            label: '지도',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('TestIcon.png'), color: Colors.black),
+            label: '뇌동맥 판단',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('CommunityIcon.png'), color: Colors.black),
+            label: '커뮤니티',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('ChatBotIcon.png'), color: Colors.black),
+            label: '챗봇',
+          ),
+        ],
+        onTap: (index) {
+          // 탭에 따른 페이지 이동 로직
+          switch (index) {
+            case 0:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const MapPage()),
+              );
+              break;
+            case 1:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ChoicePage()),
+              );
+              break;
+            case 2:
+            // 커뮤니티 페이지로 이동해야 하는 경우 여기에 해당 페이지로 이동하는 코드를 추가합니다.
+              break;
+            case 3:
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ChatBotPage()),
+              );
+              break;
+          }
+        },
       ),
     );
   }
