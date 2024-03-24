f5565d9bd2531da92bfc9117156f7fb881f86ba0
seohyeon <uimi3606@gmail.com>
2024-03-24 22:57:28 +0900

    DESIGN mainpage 디자인 수정

--- lib/InputPage.dart
+++ lib/InputPage.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:brain/ResultPage.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-// 직접 입력 페이지
+import 'MyHomePage.dart'; // json.encode를 사용하기 위해 import합니다.
+
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
 
@@ -10,63 +13,193 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  TextEditingController _systolicBloodPressureController = TextEditingController();
-  TextEditingController _fastingBloodSugarController = TextEditingController();
-
-
-
+  final TextEditingController baseYearController = TextEditingController();
+  final TextEditingController subscriberIdController = TextEditingController();
+  final TextEditingController cityCodeController = TextEditingController();
+  final TextEditingController genderCodeController = TextEditingController();
+  final TextEditingController ageGroupCodeController = TextEditingController();
+  final TextEditingController heightController = TextEditingController();
+  final TextEditingController weightController = TextEditingController();
+  final TextEditingController waistCircumferenceController = TextEditingController();
+  final TextEditingController leftEyeSightController = TextEditingController();
+  final TextEditingController rightEyeSightController = TextEditingController();
+  final TextEditingController leftHearingController = TextEditingController();
+  final TextEditingController rightHearingController = TextEditingController();
+  final TextEditingController systolicBloodPressureController = TextEditingController();
+  final TextEditingController diastolicBloodPressureController = TextEditingController();
+  final TextEditingController fastingBloodSugarController = TextEditingController();
+  final TextEditingController totalCholesterolController = TextEditingController();
+  final TextEditingController triglyceridesController = TextEditingController();
+  final TextEditingController hdlCholesterolController = TextEditingController();
+  final TextEditingController ldlCholesterolController = TextEditingController();
+  final TextEditingController hemoglobinController = TextEditingController();
+  final TextEditingController urineProteinController = TextEditingController();
+  final TextEditingController serumCreatinineController = TextEditingController();
+  final TextEditingController astController = TextEditingController();
+  final TextEditingController altController = TextEditingController();
+  final TextEditingController gammaGtpController = TextEditingController();
+  final TextEditingController smokingStatusController = TextEditingController();
+  final TextEditingController drinkingStatusController = TextEditingController();
+  final TextEditingController oralExamController = TextEditingController();
+  final TextEditingController dentalCariesController = TextEditingController();
+  final TextEditingController missingTeethController = TextEditingController();
+  final TextEditingController dentalAbnormalitiesController = TextEditingController();
+  final TextEditingController thirdMolarAbnormalitiesController = TextEditingController();
+  final TextEditingController tartarController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('직접 입력 페이지'),
+        backgroundColor: Color(0xFF1C2541),
+        title: InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const MyHomePage()),
+            );
+          },
+          child: Image.asset('../assets/HeaderLogo.png'),
+        ),
+        centerTitle: true,
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            TextFormField(decoration: const InputDecoration(labelText: '기준년도')),
-            TextFormField(decoration: const InputDecoration(labelText: '가입자 일련번호')),
-            TextFormField(decoration: const InputDecoration(labelText: '시도코드')),
-            TextFormField(decoration: const InputDecoration(labelText: '성별코드')),
-            TextFormField(decoration: const InputDecoration(labelText: '연령대코드(5세 단위)')),
-            TextFormField(decoration: const InputDecoration(labelText: '신장(5cm 단위)')),
-            TextFormField(decoration: const InputDecoration(labelText: '체중(5kg 단위)')),
-            TextFormField(decoration: const InputDecoration(labelText: '허리둘레')),
-            TextFormField(decoration: const InputDecoration(labelText: '시력(좌)')),
-            TextFormField(decoration: const InputDecoration(labelText: '시력(우)')),
-            TextFormField(decoration: const InputDecoration(labelText: '청력(좌)')),
-            TextFormField(decoration: const InputDecoration(labelText: '청력(우)')),
             TextFormField(
-              controller: _systolicBloodPressureController,
+              controller: baseYearController,
+              decoration: const InputDecoration(labelText: '기준 년도'),
+            ),
+            TextFormField(
+              controller: subscriberIdController,
+              decoration: const InputDecoration(labelText: '가입자 일련번호'),
+            ),
+            TextFormField(
+              controller: cityCodeController,
+              decoration: const InputDecoration(labelText: '시도 코드'),
+            ),
+            TextFormField(
+              controller: genderCodeController,
+              decoration: const InputDecoration(labelText: '성별 코드'),
+            ),
+            TextFormField(
+              controller: ageGroupCodeController,
+              decoration: const InputDecoration(labelText: '연령대 코드'),
+            ),
+            TextFormField(
+              controller: heightController,
+              decoration: const InputDecoration(labelText: '신장'),
+            ),
+            TextFormField(
+              controller: weightController,
+              decoration: const InputDecoration(labelText: '체중'),
+            ),
+            TextFormField(
+              controller: waistCircumferenceController,
+              decoration: const InputDecoration(labelText: '허리 둘레'),
+            ),
+            TextFormField(
+              controller: leftEyeSightController,
+              decoration: const InputDecoration(labelText: '시력(좌)'),
+            ),
+            TextFormField(
+              controller: rightEyeSightController,
+              decoration: const InputDecoration(labelText: '시력(우)'),
+            ),
+            TextFormField(
+              controller: leftHearingController,
+              decoration: const InputDecoration(labelText: '청력(좌)'),
+            ),
+            TextFormField(
+              controller: rightHearingController,
+              decoration: const InputDecoration(labelText: '청력(우)'),
+            ),
+            TextFormField(
+              controller: systolicBloodPressureController,
               decoration: const InputDecoration(labelText: '수축기 혈압'),
             ),
-            TextFormField(decoration: const InputDecoration(labelText: '이완기 혈압')),
             TextFormField(
-              controller: _fastingBloodSugarController,
+              controller: diastolicBloodPressureController,
+              decoration: const InputDecoration(labelText: '이완기 혈압'),
+            ),
+            TextFormField(
+              controller: fastingBloodSugarController,
               decoration: const InputDecoration(labelText: '식전 혈당'),
             ),
-            TextFormField(decoration: const InputDecoration(labelText: '총 콜레스테롤')),
-            TextFormField(decoration: const InputDecoration(labelText: '트리글리세라이드')),
-            TextFormField(decoration: const InputDecoration(labelText: '콜레스테롤(HDL)')),
-            TextFormField(decoration: const InputDecoration(labelText: '콜레스테롤(LDL)')),
-            TextFormField(decoration: const InputDecoration(labelText: '혈색소')),
-            TextFormField(decoration: const InputDecoration(labelText: '요단백')),
-            TextFormField(decoration: const InputDecoration(labelText: '혈청크레아티닌')),
-            TextFormField(decoration: const InputDecoration(labelText: '간기능검사(AST)')),
-            TextFormField(decoration: const InputDecoration(labelText: '간기능검사(ALT)')),
-            TextFormField(decoration: const InputDecoration(labelText: '감마지티피')),
-            TextFormField(decoration: const InputDecoration(labelText: '흡연상태')),
-            TextFormField(decoration: const InputDecoration(labelText: '음주여부')),
-            TextFormField(decoration: const InputDecoration(labelText: '구강검진수검여부')),
-            TextFormField(decoration: const InputDecoration(labelText: '치아우식증유무')),
-            TextFormField(decoration: const InputDecoration(labelText: '결손치유무')),
-            TextFormField(decoration: const InputDecoration(labelText: '치아마모증유무')),
-            TextFormField(decoration: const InputDecoration(labelText: '제3대구치(사랑니)이상')),
-            TextFormField(decoration: const InputDecoration(labelText: '치석')),
-            // 필요에 따라 추가 필드를 계속 추가할 수 있습니다.
+            TextFormField(
+              controller: totalCholesterolController,
+              decoration: const InputDecoration(labelText: '총 콜레스테롤'),
+            ),
+            TextFormField(
+              controller: triglyceridesController,
+              decoration: const InputDecoration(labelText: '트리글리세라이드'),
+            ),
+            TextFormField(
+              controller: hdlCholesterolController,
+              decoration: const InputDecoration(labelText: 'HDL 콜레스테롤'),
+            ),
+            TextFormField(
+              controller: ldlCholesterolController,
+              decoration: const InputDecoration(labelText: 'LDL 콜레스테롤'),
+            ),
+            TextFormField(
+              controller: hemoglobinController,
+              decoration: const InputDecoration(labelText: '혈색소'),
+            ),
+            TextFormField(
+              controller: urineProteinController,
+              decoration: const InputDecoration(labelText: '요단백'),
+            ),
+            TextFormField(
+              controller: serumCreatinineController,
+              decoration: const InputDecoration(labelText: '혈청 크레아티닌'),
+            ),
+            TextFormField(
+              controller: astController,
+              decoration: const InputDecoration(labelText: '간기능검사 AST'),
+            ),
+            TextFormField(
+              controller: altController,
+              decoration: const InputDecoration(labelText: '간기능검사 ALT'),
+            ),
+            TextFormField(
+              controller: gammaGtpController,
+              decoration: const InputDecoration(labelText: '감마지티피'),
+            ),
+            TextFormField(
+              controller: smokingStatusController,
+              decoration: const InputDecoration(labelText: '흡연상태'),
+            ),
+            TextFormField(
+              controller: drinkingStatusController,
+              decoration: const InputDecoration(labelText: '음주여부'),
+            ),
+            TextFormField(
+              controller: oralExamController,
+              decoration: const InputDecoration(labelText: '구강검진 수검 여부'),
+            ),
+            TextFormField(
+              controller: dentalCariesController,
+              decoration: const InputDecoration(labelText: '치아 우식증 유무'),
+            ),
+            TextFormField(
+              controller: missingTeethController,
+              decoration: const InputDecoration(labelText: '결손치 유무'),
+            ),
+            TextFormField(
+              controller: dentalAbnormalitiesController,
+              decoration: const InputDecoration(labelText: '치아 마모증 유무'),
+            ),
+            TextFormField(
+              controller: thirdMolarAbnormalitiesController,
+              decoration: const InputDecoration(labelText: '제3대구치 이상'),
+            ),
+            TextFormField(
+              controller: tartarController,
+              decoration: const InputDecoration(labelText: '치석'),
+            ),
           ],
         ),
       ),
@@ -75,22 +208,88 @@ class _InputPageState extends State<InputPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ResultPage(
-                      systolicBloodPressure: _systolicBloodPressureController.text,
-                      fastingBloodSugar: _fastingBloodSugarController.text,
-                    ),
-                  ),
-                );
+              onPressed: () async {
+                // 각 폼 필드의 값들을 Map 형태로 저장합니다.
+                Map<String, dynamic> inputData = {
+                  '기준년도': baseYearController.text,
+                  '가입자_일련번호': subscriberIdController.text,
+                  '시도코드': cityCodeController.text,
+                  '성별코드': genderCodeController.text,
+                  '연령대코드': ageGroupCodeController.text,
+                  '신장': heightController.text,
+                  '체중': weightController.text,
+                  '허리둘레': waistCircumferenceController.text,
+                  '시력_좌': leftEyeSightController.text,
+                  '시력_우': rightEyeSightController.text,
+                  '청력_좌': leftHearingController.text,
+                  '청력_우': rightHearingController.text,
+                  '수축기_혈압': systolicBloodPressureController.text,
+                  '이완기_혈압': diastolicBloodPressureController.text,
+                  '식전혈당': fastingBloodSugarController.text,
+                  '총_콜레스테롤': totalCholesterolController.text,
+                  '트리글리세라이드': triglyceridesController.text,
+                  '콜레스테롤_HDL': hdlCholesterolController.text,
+                  '콜레스테롤_LDL': ldlCholesterolController.text,
+                  '혈색소': hemoglobinController.text,
+                  '요단백': urineProteinController.text,
+                  '혈청크레아티닌': serumCreatinineController.text,
+                  '간기능검사_AST': astController.text,
+                  '간기능검사_ALT': altController.text,
+                  '감마지티피': gammaGtpController.text,
+                  '흡연상태': smokingStatusController.text,
+                  '음주여부': drinkingStatusController.text,
+                  '구강검진수검여부': oralExamController.text,
+                  '치아우식증유무': dentalCariesController.text,
+                  '결손치유무': missingTeethController.text,
+                  '치아마모증유무': dentalAbnormalitiesController.text,
+                  '제3대구치_이상': thirdMolarAbnormalitiesController.text,
+                  '치석': tartarController.text,
+                };
+
+                // 서버로 데이터를 전송합니다.
+                bool success = await sendDataToServer(inputData);
+
+                if (success) {
+                  // 데이터가 성공적으로 전송된 경우 결과 페이지로 이동합니다.
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const ResultPage(fastingBloodSugar: '', systolicBloodPressure: '',)),
+                  );
+                } else {
+                  // 데이터 전송에 실패한 경우 알림을 표시합니다.
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('데이터 전송에 실패했습니다.')),
+                  );
+                }
               },
-              child: const Text('결과보기'),
+              child:
+              const Text('결과보기'),
             ),
           ],
         ),
       ),
     );
+  }
+}
+Future<bool> sendDataToServer(Map<String, dynamic> healthRecord) async {
+  final url = Uri.parse('http://localhost:8080/save-data');
+
+  try {
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'}, // JSON 형식으로 데이터를 전송하기 위한 헤더를 추가합니다.
+      body: json.encode(healthRecord), // Map을 JSON 문자열로 변환하여 전송합니다.
+    );
+
+    if (response.statusCode == 200) {
+      print('데이터가 성공적으로 전송되었습니다.');
+      return true; // 데이터 전송 성공 시 true 반환
+    } else {
+      print('데이터 전송에 실패했습니다.');
+      return false; // 데이터 전송 실패 시 false 반환
+    }
+  } catch (e) {
+    print('오류: $e');
+    return false; // 예외 발생 시 false 반환
   }
 }
--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -51,107 +51,23 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(80.0), // AppBar의 높이를 80.0으로 설정
-        child: AppBar(
-          title: Text(''),
-          centerTitle: true,
-          flexibleSpace: LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints) {
-              return Container(
-                padding: EdgeInsets.only(top: constraints.maxHeight * 0.1),
-                // 상단 패딩 조정
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  // 가로 방향으로 중앙 정렬
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  // 세로 방향으로 중앙 정렬
-                  children: [
-                    Expanded(
-                      // 로고가 중앙에 오도록 확장
-                      child: Center(
-                        child: IconButton(
-                          icon: Image.asset('MainLogo.png'),
-                          padding: EdgeInsets.zero,
-                          onPressed: () {},
-                        ),
-                      ),
-                    ),
-                    IconButton(
-                      // 사용자 아이콘은 오른쪽에 위치
-                      icon: Image.asset('UserIcon.png'),
-                      onPressed: () {},
-                      padding: EdgeInsets.symmetric(
-                          vertical: constraints.maxHeight * 0.1), // 세로 패딩 조정
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
-          actions: <Widget>[
-            // AppBar의 actions 속성은 여전히 사용할 수 있지만, 이 예제에서는 사용하지 않음
-            SizedBox(width: 48), // IconButton의 기본 크기와 동일한 공간 확보
-          ],
+      appBar: AppBar(
+        backgroundColor: Color(0xFF1C2541),
+        title: InkWell(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const MyHomePage()),
+            );
+          },
+          child: Image.asset('../assets/HeaderLogo.png'),
         ),
+        centerTitle: true,
       ),
       body: SingleChildScrollView(
         // 전체 body에 스크롤 기능 추가
         child: Column(
           children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.2,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text("국민건강보험"),
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.black, backgroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text('정부24'),
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.black, backgroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text('한국건강관리협회'),
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.black, backgroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
             Padding(
               padding: const EdgeInsets.only(top: 50.0, bottom: 80.0),
               // 위아래 마진 부여
@@ -172,25 +88,16 @@ class MyHomePage extends StatelessWidget {
                         margin: EdgeInsets.symmetric(horizontal: 5.0),
                         decoration: BoxDecoration(
                           color: Colors.white,
-                          borderRadius: BorderRadius.circular(10), // 모서리 둥글게
+                          borderRadius: BorderRadius.circular(30), // 모서리 둥글게
                           boxShadow: [
                             // 그림자 효과
                             BoxShadow(
                               color: Colors.grey.withOpacity(0.5),
                               spreadRadius: 2,
                               blurRadius: 5,
-                              offset:
-                                  Offset(0, 3), // changes position of shadow
+                              offset: Offset(0, 3), // changes position of shadow
                             ),
                           ],
-                        ),
-                        child: ClipRRect(
-                          // Container 내부의 모든 것들도 둥글게 처리
-                          borderRadius: BorderRadius.circular(10), // 모서리 둥글게
-                          child: Center(
-                            child:
-                                Text('박스 $i', style: TextStyle(fontSize: 16.0)),
-                          ),
                         ),
                       );
                     },
@@ -198,22 +105,64 @@ class MyHomePage extends StatelessWidget {
                 }).toList(),
               ),
             ),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.55,
-              child: GridView.count(
-                crossAxisCount: 2,
-                childAspectRatio: 1.0,
-                physics: NeverScrollableScrollPhysics(),
-                children: List.generate(4, (index) {
-                  return HoverImage(
-                    imagePath: 'mainIntro${index + 1}.png',
-                  );
-                }),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "오늘의 퀴즈",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Container(
+                    width: 380,
+                    height: 180,
+                    margin: EdgeInsets.only(bottom: 20.0),
+                    decoration: BoxDecoration(
+                      color: Color(0xFFD9D9D9),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: Container(
+                width: 380,
+                height: 220,
+                margin: EdgeInsets.only(bottom: 20.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFFD9D9D9),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: Container(
+                width: 380,
+                height: 220,
+                decoration: BoxDecoration(
+                  color: Color(0xFFD9D9D9),
+                  borderRadius: BorderRadius.circular(30),
+                ),
               ),
             ),
           ],
         ),
       ),
+
+
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         // 아이템들이 동일한 공간을 차지하도록 설정
