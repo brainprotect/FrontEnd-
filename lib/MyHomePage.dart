fd6440c26fe729eb25e2a5ac7d5112059c566f5e
seohyeon <uimi3606@gmail.com>
2024-03-25 21:31:11 +0900

    FIX 순서도 추가 및 assets폴더에 폰트 추가

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -4,7 +4,7 @@ import 'package:brain/ChoicePage.dart';
 import 'package:brain/ChatBotPage.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-
+// Pop-upPage.dart 파일이 있는 경로로 수정
 import 'Pop-upPage.dart';
 
 class HoverImage extends StatefulWidget {
@@ -53,170 +53,209 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF1C2541),
-        title: InkWell(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const MyHomePage()),
-            );
-          },
-          child: Image.asset('assets/HeaderLogo.png'), // 이미지 경로 수정
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: TextTheme(
+          bodyText1: TextStyle(
+              fontFamily: 'Sejonghospital'), // 예시로 bodyText1에 fontFamily를 적용
+          // 여기에 다른 텍스트 스타일들을 추가할 수 있습니다.
         ),
-        centerTitle: true,
       ),
-      body: SingleChildScrollView(
-        // 전체 body에 스크롤 기능 추가
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 50.0, bottom: 80.0),
-              // 위아래 마진 부여
-              child: CarouselSlider(
-                options: CarouselOptions(
-                  height: MediaQuery.of(context).size.height * 0.25,
-                  autoPlay: true,
-                  enlargeCenterPage: true,
-                  viewportFraction: 0.8,
-                  aspectRatio: 16 / 9,
-                  initialPage: 0,
-                ),
-                items: [1, 2, 3, 4].map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: EdgeInsets.symmetric(horizontal: 5.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(30), // 모서리 둥글게
-                          boxShadow: [
-                            // 그림자 효과
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 2,
-                              blurRadius: 5,
-                              offset: Offset(0, 3), // changes position of shadow
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFF1C2541),
+          title: InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const MyHomePage()),
+              );
+            },
+            child: Image.asset('assets/HeaderLogo.png'), // 이미지 경로 수정
+          ),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          // 전체 body에 스크롤 기능 추가
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 50.0, bottom: 40.0),
+                // 위아래 마진 부여
+                child: CarouselSlider(
+                  options: CarouselOptions(
+                    height: MediaQuery.of(context).size.height * 0.25,
+                    autoPlay: true,
+                    enlargeCenterPage: true,
+                    viewportFraction: 0.8,
+                    aspectRatio: 16 / 9,
+                    initialPage: 0,
+                  ),
+                  items: [
+                    "1. 메인페이지 - 사용법 안내 및 뇌동맥의 원인 및 위험성 설명",
+                    "2. 뇌동맥 판단 페이지 - 건강검진 제출 방법 선택(직접 작성, 촬영, 건강검진 결과 불러오기 중 선택)",
+                    "3. 뇌동맥류 검사 결과 출력 페이지 - 건강검진표를 통한 뇌동맥 여부 확인 및 결과 제공",
+                    "4. 지도 페이지 - 병원 위치 확인 및 검사 가능한 병원 찾기",
+                    "5. 챗봇 페이지 - 뇌동맥류 예측 및 관련 정보 제공",
+                    "6. 커뮤니티 페이지 - 뇌동맥류 관련 질환 및 치료 후기 공유",
+                  ].map((item) {
+                    return Builder(
+                      builder: (BuildContext context) {
+                        return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(30), // 모서리 둥글게
+                            boxShadow: [
+                              // 그림자 효과
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                                offset:
+                                    Offset(0, 3), // changes position of shadow
+                              ),
+                            ],
+                          ),
+                          child: Center(
+                            child: Text(
+                              item,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
                             ),
-                          ],
-                        ),
-                      );
-                    },
-                  );
-                }).toList(),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 50.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "오늘의 퀴즈",
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Container(
-                    width: 380,
-                    height: 180,
-                    margin: EdgeInsets.only(bottom: 20.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFFD9D9D9),
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-              child: Container(
-                width: 380,
-                height: 220,
-                margin: EdgeInsets.only(bottom: 20.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFFD9D9D9),
-                  borderRadius: BorderRadius.circular(30),
+                          ),
+                        );
+                      },
+                    );
+                  }).toList(),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-              child: Container(
-                width: 380,
-                height: 220,
-                decoration: BoxDecoration(
-                  color: Color(0xFFD9D9D9),
-                  borderRadius: BorderRadius.circular(30),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0, vertical: 50.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "오늘의 퀴즈",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      width: 380,
+                      height: 180,
+                      margin: EdgeInsets.only(bottom: 20.0),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFD9D9D9),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ],
                 ),
               ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                child: Container(
+                  width: 380,
+                  height: 220,
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFD9D9D9),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0, vertical: 40.0),
+                child: Container(
+                  width: 380,
+                  height: 220,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFD9D9D9),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Color(0xFF1C2541),
+          type: BottomNavigationBarType.fixed,
+          // 아이템들이 동일한 공간을 차지하도록 설정
+          selectedItemColor: Colors.white,
+          // 선택된 아이템의 색상을 검정으로 설정
+          unselectedItemColor: Colors.white,
+          // 선택되지 않은 아이템의 색상을 검정으로 설정
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/mapIcon.png'),
+                  color: Colors.white),
+              // 이미지 경로 수정
+              label: '지도',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/TestIcon.png'),
+                  color: Colors.white),
+              // 이미지 경로 수정
+              label: '뇌동맥 판단',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/CommunityIcon.png'),
+                  color: Colors.white),
+              // 이미지 경로 수정
+              label: '커뮤니티',
+            ),
+            BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage('assets/ChatBotIcon.png'),
+                  color: Colors.white),
+              // 이미지 경로 수정
+              label: '챗봇',
             ),
           ],
+          onTap: (index) {
+            // 탭에 따른 페이지 이동 로직
+            switch (index) {
+              case 0:
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MapPage()),
+                );
+                break;
+              case 1:
+                // 뇌동맥 판단 버튼을 눌렀을 때 동작
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return BrainAssessmentPopup();
+                  },
+                );
+                break;
+              case 2:
+                // 커뮤니티 페이지로 이동해야 하는 경우 여기에 해당 페이지로 이동하는 코드를 추가합니다.
+                break;
+              case 3:
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ChatBotPage()),
+                );
+                break;
+            }
+          },
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Color(0xFF1C2541),
-        type: BottomNavigationBarType.fixed, // 아이템들이 동일한 공간을 차지하도록 설정
-        selectedItemColor: Colors.white, // 선택된 아이템의 색상을 검정으로 설정
-        unselectedItemColor: Colors.white, // 선택되지 않은 아이템의 색상을 검정으로 설정
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: ImageIcon(AssetImage('assets/mapIcon.png'), color: Colors.white), // 이미지 경로 수정
-            label: '지도',
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(AssetImage('assets/TestIcon.png'), color: Colors.white), // 이미지 경로 수정
-            label: '뇌동맥 판단',
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(AssetImage('assets/CommunityIcon.png'), color: Colors.white), // 이미지 경로 수정
-            label: '커뮤니티',
-          ),
-          BottomNavigationBarItem(
-            icon: ImageIcon(AssetImage('assets/ChatBotIcon.png'), color: Colors.white), // 이미지 경로 수정
-            label: '챗봇',
-          ),
-        ],
-        onTap: (index) {
-          // 탭에 따른 페이지 이동 로직
-          switch (index) {
-            case 0:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const MapPage()),
-              );
-              break;
-            case 1:
-            // 뇌동맥 판단 버튼을 눌렀을 때 동작
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return BrainAssessmentPopup();
-                },
-              );
-              break;
-
-            case 2:
-            // 커뮤니티 페이지로 이동해야 하는 경우 여기에 해당 페이지로 이동하는 코드를 추가합니다.
-              break;
-            case 3:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const ChatBotPage()),
-              );
-              break;
-          }
-        },
       ),
     );
   }
